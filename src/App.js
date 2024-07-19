e2ff4ff8da11e5f9b09da692beb34a0759313abd
Muhammd Yousaf <yousafva9@gmail.com>
2024-07-19 07:56:26 -0700

    updated

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PasswordUI from './PasswordGenerator';
+
+function App() {
+  return (
+    <div className="App">
+      <PasswordUI/>
+    </div>
+  );
+}
+
+export default App;
